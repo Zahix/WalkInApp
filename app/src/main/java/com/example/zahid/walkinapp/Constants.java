1ec3b76ed84d34121f203e67384ff6483afa9b84
ZahidOmer <pakomzoo@gmail.com>
2019-05-29 01:00:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/zahid/walkinapp/Constants.java
@@ -0,0 +1,14 @@
+package com.example.zahid.walkinapp;
+
+/**
+ * Created by Zahid on 29/05/2019.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+
+    public static final int CONFIDENCE = 70;
+}
---
+++ app/src/main/java/com/example/zahid/walkinapp/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.zahid.walkinapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.location.DetectedActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    BroadcastReceiver broadcastReceiver;
+
+    private TextView txtActivity, txtConfidence;
+    //private ImageView imgActivity;
+    private Button btnStartTrcking, btnStopTracking;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtActivity = findViewById(R.id.txt_activity);
+        txtConfidence = findViewById(R.id.txt_confidence);
+        //imgActivity = findViewById(R.id.img_activity);
+        btnStartTrcking = findViewById(R.id.btn_start_tracking);
+        btnStopTracking = findViewById(R.id.btn_stop_tracking);
+
+        btnStartTrcking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTracking();
+            }
+        });
+
+        btnStopTracking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopTracking();
+            }
+        });
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(Constants.BROADCAST_DETECTED_ACTIVITY)) {
+                    int type = intent.getIntExtra("type", -1);
+                    int confidence = intent.getIntExtra("confidence", 0);
+                    handleUserActivity(type, confidence);
+                }
+            }
+        };
+
+        startTracking();
+    }
+
+    private void handleUserActivity(int type, int confidence) {
+        String label = "Unkwons";
+       // int icon = R.drawable.ic_still;
+
+        switch (type) {
+            case DetectedActivity.IN_VEHICLE: {
+                label = "Vehicle";
+               // icon = R.drawable.ic_driving;
+                break;
+            }
+            case DetectedActivity.ON_BICYCLE: {
+                label = "Bicycle";
+                //icon = R.drawable.ic_on_bicycle;
+                break;
+            }
+            case DetectedActivity.ON_FOOT: {
+                label = "On foot";
+                //icon = R.drawable.ic_walking;
+                break;
+            }
+            case DetectedActivity.RUNNING: {
+                label = "Running";
+                //icon = R.drawable.ic_running;
+                break;
+            }
+            case DetectedActivity.STILL: {
+                label = "No Move";
+                break;
+            }
+            case DetectedActivity.TILTING: {
+                label = "Tilting Move";
+                //icon = R.drawable.ic_tilting;
+                break;
+            }
+            case DetectedActivity.WALKING: {
+                label = "Walking";
+               // icon = R.drawable.ic_walking;
+                break;
+            }
+            case DetectedActivity.UNKNOWN: {
+                label = "Unknow";
+                break;
+            }
+        }
+
+        Log.e(TAG, "User activity: " + label + ", Confidence: " + confidence);
+
+        if (confidence > Constants.CONFIDENCE) {
+            txtActivity.setText(label);
+            txtConfidence.setText("Confidence: " + confidence);
+            //imgActivity.setImageResource(icon);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
+                new IntentFilter(Constants.BROADCAST_DETECTED_ACTIVITY));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+    }
+
+    private void startTracking() {
+        Intent intent1 = new Intent(MainActivity.this, BackgroundDetectedActivitiesService.class);
+        startService(intent1);
+    }
+
+    private void stopTracking() {
+        Intent intent = new Intent(MainActivity.this, BackgroundDetectedActivitiesService.class);
+        stopService(intent);
+    }
+}
